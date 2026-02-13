535cf3ab3585f01ba4b534426adfd347738aaf29
Maikel Galvao <maikelgalvao182@gmail.com>
2026-02-13 13:53:33 -0300

    Add X pixel base code and conversion tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { getLocale } from "next-intl/server";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -18,7 +19,46 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        <Script id="twitter-uwt-base" strategy="beforeInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','r55je');
+          `}
+        </Script>
+      </head>
       <body className={inter.variable} suppressHydrationWarning>
+        <Script id="twitter-conversion-event" strategy="afterInteractive">
+          {`
+            twq('event', 'tw-r55je-r55pf', {
+              value: null,
+              currency: null,
+              contents: [
+                {
+                  content_type: null,
+                  content_id: null,
+                  content_name: null,
+                  content_price: null,
+                  num_items: null,
+                  content_group_id: null
+                },
+                {
+                  content_type: null,
+                  content_id: null,
+                  content_name: null,
+                  content_price: null,
+                  num_items: null,
+                  content_group_id: null
+                }
+              ],
+              conversion_id: null,
+              email_address: null,
+              phone_number: null
+            });
+          `}
+        </Script>
         {children}
         <Analytics />
       </body>
